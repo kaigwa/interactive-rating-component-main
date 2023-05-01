d8963b3f1fcb40e25701f4ee474a08d9fb20e85c
Reset <kaigwaedwin@gmail.com>
2023-05-01 20:01:42 +0300

    New Features

---
+++ index.js
@@ -0,0 +1,19 @@
+// creation of all functions
+const mainContainer = document.querySelector('.container');
+const thankContainer = document.querySelector('.thank');
+const submitButton = document.getElementById('button');
+const vote = document.getElementById('vote');
+const rates = document.querySelectorAll('.bttn')
+
+// call on submit button and manipuration of container pagees
+submitButton.addEventListener('click', () => {
+    thankContainer.classList.remove('hidden')
+    mainContainer.style.display = 'none';
+})
+
+// call and declaration of vote-button
+rates.forEach((rate) => {
+ rate.addEventListener('click', () =>{
+    vote.innerHTML = rate.innerHTML
+ })   
+});
